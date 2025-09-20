ec7786208e476dc3bc7d0b95b992ecfe7f57fe35
Johannes Brix <johannes.brix7@gmail.com>
2025-09-20 14:47:13 +0200

    adjusted database schema

--- db/schema/users-schema.ts
+++ db/schema/users-schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid, boolean } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
@@ -6,6 +6,8 @@ export const users = pgTable("users", {
   email: text("email").notNull(),
   first_name: text("first_name"),
   last_name: text("last_name"),
+  show_on_leaderboard: boolean("show_on_leaderboard").default(true).notNull(),
+  show_posts_in_feed: boolean("show_posts_in_feed").default(true).notNull(),
   created_at: timestamp("created_at").defaultNow().notNull(),
   updated_at: timestamp("updated_at").defaultNow().notNull().$onUpdate(() => new Date()),
 });
