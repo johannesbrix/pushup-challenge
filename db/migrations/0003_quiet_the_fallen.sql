ae1e8762325842e53cf39b7fdc74f7d8a502dc19
Johannes Brix <johannes.brix7@gmail.com>
2025-09-20 14:49:58 +0200

    migrate dataschema

---
+++ db/migrations/0003_quiet_the_fallen.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "users" ADD COLUMN "show_on_leaderboard" boolean DEFAULT true NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "show_posts_in_feed" boolean DEFAULT true NOT NULL;
